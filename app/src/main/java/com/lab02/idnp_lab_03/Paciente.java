4e95432cf728c7d6b09a15b6645adf3ac26e1ee8
Arnold <amolloa@unsa.edu.pe>
2021-10-05 10:38:21 -0500

    XML de Paciente

--- app/src/main/java/com/lab02/idnp_lab_03/Paciente.java
+++ app/src/main/java/com/lab02/idnp_lab_03/Paciente.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Paciente extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //comment
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_paciente);
     }
 }
