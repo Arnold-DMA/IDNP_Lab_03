c7c819c4a7fd06213872483c24fb4fbab6a71ca5
Arnold <amolloa@unsa.edu.pe>
2021-10-05 12:52:34 -0500

    Activity Paciente funcionando

--- app/src/main/java/com/lab02/idnp_lab_03/MainActivity.java
+++ app/src/main/java/com/lab02/idnp_lab_03/MainActivity.java
@@ -2,14 +2,89 @@ package com.lab02.idnp_lab_03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> arrayDNI, arrayNombre, arrayApellido, arrayDireccion;
+    TextView textViewDatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //comment
         setContentView(R.layout.activity_main);
+
+        arrayDNI = new ArrayList<String>();
+        arrayNombre = new ArrayList<String>();
+        arrayApellido = new ArrayList<String>();
+        arrayDireccion = new ArrayList<String>();
+        //visitas = new ArrayList<String[]>();
+
+        textViewDatos = findViewById(R.id.textViewDatos);
+        Button btnPaciente = findViewById(R.id.btnPaciente);
+        btnPaciente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pacienteIntent = new Intent(getApplicationContext(), Paciente.class);
+                /*Bundle arrayPacientes = new Bundle();
+                arrayPacientes.putStringArrayList("DNI", arrayDNI);
+                arrayPacientes.putStringArrayList("Nombre", arrayNombre);
+                arrayPacientes.putStringArrayList("Apellido", arrayApellido);
+                arrayPacientes.putStringArrayList("Dirección", arrayDireccion);
+                pacienteIntent.putExtras(arrayPacientes);*/
+                startActivity(pacienteIntent);
+            }
+        });
+
+
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Bundle parametros = this.getIntent().getExtras();
+        if(parametros != null){
+            arrayDNI.add(parametros.getString("DNI"));
+            arrayNombre.add(parametros.getString("Nombre"));
+            arrayApellido.add(parametros.getString("Apellido"));
+            arrayDireccion.add(parametros.getString("Direccion"));
+        }
+        int cantidadPacientes = arrayDNI.size();
+        if(cantidadPacientes != 0){
+            textViewDatos.setText("Datos de paciente"+
+                    "\nDNI: "+arrayDNI.get(cantidadPacientes - 1)+
+                    "\nNombre: "+arrayNombre.get(cantidadPacientes - 1)+
+                    "\nApellido: "+arrayApellido.get(cantidadPacientes - 1)+
+                    "\nDirección: "+arrayDireccion.get(cantidadPacientes - 1));
+        }
+        else{
+            textViewDatos.setText("Datos de paciente");
+        }
+
+    }
+
+    /*@Override
+    protected void onRestart() {
+        super.onRestart();
+
+        /*Bundle parametros = this.getIntent().getExtras();
+        if(parametros != null){
+            arrayDNI.add(parametros.getString("DNI"));
+        }
+
+        //int cantidadPacientes = arrayDNI.size();
+        /*if(cantidadPacientes != 0){
+            textViewDatos.setText("Hola");
+        }
+        else{
+            textViewDatos.setText("adios");
+        }
+    }*/
 }
--- app/src/main/java/com/lab02/idnp_lab_03/Paciente.java
+++ app/src/main/java/com/lab02/idnp_lab_03/Paciente.java
@@ -2,13 +2,52 @@ package com.lab02.idnp_lab_03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class Paciente extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paciente);
+        /*ArrayList<String> arrayDNI = getIntent().getExtras().getStringArrayList("DNI");
+        ArrayList<String> arrayNombre = getIntent().getExtras().getStringArrayList("Nombre");
+        ArrayList<String> arrayApellido = getIntent().getExtras().getStringArrayList("Apellido");
+        ArrayList<String> arrayDireccion = getIntent().getExtras().getStringArrayList("Direccion");*/
+
+        EditText editTextDNI = findViewById(R.id.editTextDNI);
+        EditText editTextNombre = findViewById(R.id.editTextNombre);
+        EditText editTextApellido = findViewById(R.id.editTextApellido);
+        EditText editTextDireccion = findViewById(R.id.editTextDireccion);
+
+        Button btnRegistrar = findViewById(R.id.btnRegistrar);
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*arrayDNI.add(editTextDNI.getText().toString());
+                arrayNombre.add(editTextNombre.getText().toString());
+                arrayApellido.add(editTextApellido.getText().toString());
+                arrayDireccion.add(editTextDireccion.getText().toString());*/
+                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                Bundle arrayPacientes = new Bundle();
+                arrayPacientes.putString("DNI", editTextDNI.getText().toString());
+                arrayPacientes.putString("Nombre", editTextNombre.getText().toString());
+                arrayPacientes.putString("Apellido", editTextApellido.getText().toString());
+                arrayPacientes.putString("Direccion", editTextDireccion.getText().toString());
+                mainIntent.putExtras(arrayPacientes);
+                startActivity(mainIntent);
+
+            }
+        });
+
+
     }
 }
